92b4e4f936e3621fc885d1988dbf44a8a9394b05
Nathan Raine <nraine18@gmail.com>
2026-04-26 11:24:59 -0500

    Better title header

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,7 +3,16 @@ import Grid from "./Grid";
 export default function Game() {
     return (
         <>
-            <h1>{"Varsity Grid"}</h1>
+            <h1
+                style={{
+                    "color": "#dedede",
+                    "fontFamily": "'Chakra Petch', sans-serif",
+                    "fontSize": "50px",
+                    "fontWeight": "700"
+                }}
+            >
+                {"Varsity Grid"}
+            </h1>
             <Grid />
         </>
     );
