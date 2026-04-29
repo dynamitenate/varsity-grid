835659cb100fde226f53b2fa150c14e22e9d3b79
Nathan Raine <nraine18@gmail.com>
2026-04-29 18:11:11 -0500

    Simplified layout

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,45 +1,46 @@
 import Cell, { HeaderCell, GhostCell } from "./Cell";
 import "./Grid.css";
 
+let categories = {
+    columns: {
+        c1: "c1",
+        c2: "c2",
+        c3: "c3"
+    },
+    rows: {
+        r1: "r1",
+        r2: "r2",
+        r3: "r3"
+    },
+}
+
 export default function Grid() {
-    let categories = {
-        columns: {
-            c1: "c1",
-            c2: "c2",
-            c3: "c3"
-        },
-        rows: {
-            r1: "r1",
-            r2: "r2",
-            r3: "r3"
-        },
-    }
     return (
         <div className="grid-body">
             <div className="grid-table">
                 <div className="grid-row">
-                    <div className="grid-table-cell"><GhostCell /></div>
-                    <div className="grid-table-cell"><HeaderCell text={categories.columns.c1} /></div>
-                    <div className="grid-table-cell"><HeaderCell text={categories.columns.c2} /></div>
-                    <div className="grid-table-cell"><HeaderCell text={categories.columns.c3} /></div>
+                    <GhostCell />
+                    <HeaderCell text={categories.columns.c1} />
+                    <HeaderCell text={categories.columns.c2} />
+                    <HeaderCell text={categories.columns.c3} />
                 </div>
                 <div className="grid-row">
-                    <div className="grid-table-cell"><HeaderCell text={categories.rows.r1} /></div>
-                    <div className="grid-table-cell"><Cell /></div>
-                    <div className="grid-table-cell"><Cell /></div>
-                    <div className="grid-table-cell"><Cell /></div>
+                    <HeaderCell text={categories.rows.r1} />
+                    <Cell />
+                    <Cell />
+                    <Cell />
                 </div>
                 <div className="grid-row">
-                    <div className="grid-table-cell"><HeaderCell text={categories.rows.r2} /></div>
-                    <div className="grid-table-cell"><Cell /></div>
-                    <div className="grid-table-cell"><Cell /></div>
-                    <div className="grid-table-cell"><Cell /></div>
+                    <HeaderCell text={categories.rows.r2} />
+                    <Cell />
+                    <Cell />
+                    <Cell />
                 </div>
                 <div className="grid-row">
-                    <div className="grid-table-cell"><HeaderCell text={categories.rows.r3} /></div>
-                    <div className="grid-table-cell"><Cell /></div>
-                    <div className="grid-table-cell"><Cell /></div>
-                    <div className="grid-table-cell"><Cell /></div>
+                    <HeaderCell text={categories.rows.r3} />
+                    <Cell />
+                    <Cell />
+                    <Cell />
                 </div>
             </div>
         </div>
